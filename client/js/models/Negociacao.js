70960fdd80006d2dcad7e05631a44170b5b31970
Hugo Fernando da Mota <hugo.mota@mvrec.local>
2018-08-27 13:32:13 -0300

    Aula2-video-6

--- client/js/models/Negociacao.js
+++ client/js/models/Negociacao.js
@@ -4,6 +4,8 @@ class Negociacao{
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;   
+
+        Object.freeze(this);//Congela o objeto para não poder mais alterar suas propriedades.
     }
 
     get data(){
