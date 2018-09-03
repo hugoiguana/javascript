4d97a59e09fac207e1f0eacd07e524c81c9e66f7
Hugo Fernando da Mota <hugo.mota@mvrec.local>
2018-09-03 13:32:56 -0300

    Aula6-video-3

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,8 +1,8 @@
 
-class NegociacoesView {
+class NegociacoesView extends View{
 
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model){
@@ -36,10 +36,5 @@ class NegociacoesView {
         </table>        
         `;
     }
-
-    update(model){
-        this._elemento.innerHTML = this._template(model);
-    }
-
 }
 
